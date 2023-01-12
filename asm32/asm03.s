cf4400226b5e366bd847a9492d4613994d8ba457
maximedbs1 <maxime.dubus0@gmail.com>
2023-01-12 15:29:24 +0100

    asm03 v2

--- asm32/asm03.s
+++ asm32/asm03.s
@@ -1,47 +1,48 @@
 section .data
-    prompt db 'Enter a number:',0
-    comp db '42'
-    messagedb '1337'
-    len equ $-message
+    message db "Entrer a number:",0
+    len_message equ $-message
+    correct_msg db "1337",0
+    len_correct_msg equ $-correct_msg
+    input_message db 2
 
 section .text
     global _start
 
 _start:
-    ; Print the prompt
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, prompt
-    mov edx, 13
-    int 80h
-
-    ; Read input into ebx
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, ebx
-    mov edx, 2
-    int 80h
-
-    ; Compare input to 42
-    cmp ebx, comp
-    je equal
-
-    ; Input is not equal to 42, return 1
-    mov eax, 1
-    mov ebx, 1
-    int 80h
-
-equal:
-    ; Input is equal to 42
-
     ; write message to stdout
-    mov edx, len
+    mov edx, len_message
     mov ecx, message
     mov ebx, 1
     mov eax, 4
-    int 80h
+    int 0x80
+
+    ; read input from stdin
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, input_message
+    mov edx, 8
+    int 0x80
+
+    ; check if input is "42"
+    cmp byte [input_message], '4'
+    jne incorrect
+    cmp byte [input_message+1], '2'
+    jne incorrect
+
+    ; write "Correct" to stdout
+    mov edx, len_correct_msg
+    mov ecx, correct_msg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
 
     ; return 0
     mov eax, 1
     mov ebx, 0
-    int 80h
+    int 0x80
+
+incorrect:
+    ; return 1
+    mov eax, 1
+    mov ebx, 1
+    int 0x80
